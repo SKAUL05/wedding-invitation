19f77268e9abbcfb911bacce9faf0933c747cfe9
SKAUL05 <kaul.sarath@gmail.com>
2022-07-22 22:34:39 +0530

    Assets Added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 3, 2022 10:00:00").getTime();
+var countDownDate = new Date("Dec 3, 2022 11:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -27,7 +27,11 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + 
+                                                "<br>Days</div>" + "<div class='hours block'>" + hours + 
+                                                "<br>Hours</div>" + "<div class='minutes block'>" + minutes + 
+                                                "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + 
+                                                "<br>Seconds</div></div>";
     
     // If the count down is over, write some text 
     if (distance < 0) {
@@ -37,36 +41,36 @@ var x = setInterval(function() {
 }, 1000);
 
 // being a bit cool :p  
-var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+// var styles = [
+//     'background: linear-gradient(#D33106, #571402)'
+//     , 'border: 4px solid #3E0E02'
+//     , 'color: red'
+//     , 'display: block'
+//     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
+//     , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+//     , 'line-height: 40px'
+//     , 'text-align: center'
+//     , 'font-weight: bold'
+//     , 'font-size: 32px'
+// ].join(';');
 
-var styles1 = [
-    'color: #FF6C37'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+// var styles1 = [
+//     'color: #FF6C37'
+//     , 'display: block'
+//     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+//     , 'line-height: 40px'
+//     , 'font-weight: bold'
+//     , 'font-size: 32px'
+// ].join(';');
 
-var styles2 = [
-    'color: teal'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+// var styles2 = [
+//     'color: teal'
+//     , 'display: block'
+//     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+//     , 'line-height: 40px'
+//     , 'font-weight: bold'
+//     , 'font-size: 32px'
+// ].join(';');
 
 console.log('\n\n%c SAVE THE DATE: 3rd Dec, 2022!', styles);
 
